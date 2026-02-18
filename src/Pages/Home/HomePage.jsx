1ee0d72f07b2a57de2e687dbe5fa5eb4a16acf94
Sugaam-dev <pamirnayak6@gmail.com>
2026-02-18 09:41:26 +0530

    home page optimized

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,25 +1,25 @@
-
-
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
 import './HomePage.css';
-import HeroCarousel from './HeroCarousel';
-import FeaturesSection from './FeaturesSection';
-import GuidanceSection from './GuidanceSection';
-import DesignStudio from './DesignStudio';
-import Testimonial from './Testimonial';
-import EnquiryPage from './EnquiryPage';
+
+const HeroCarousel = lazy(() => import('./HeroCarousel'));
+const FeaturesSection = lazy(() => import('../../components/FeaturesSection'));
+const GuidanceSection = lazy(() => import('./GuidanceSection'));
+const DesignStudio = lazy(() => import('./DesignStudio'));
+const Testimonial = lazy(() => import('./Testimonial'));
+const EnquiryPage = lazy(() => import('./EnquiryPage'));
 
 const HomePage = () => {
-
   return (
     <div className="home-content-container">
-      <HeroCarousel />
-      <FeaturesSection />
-      <GuidanceSection />
-      <DesignStudio />
-      <Testimonial />
-      <EnquiryPage />
+     
+        <HeroCarousel />
+         <Suspense fallback={<div className="loader">Loading...</div>}>
+        <FeaturesSection/>
+        <GuidanceSection />
+        <DesignStudio />
+        <Testimonial />
+        <EnquiryPage />
+      </Suspense>
     </div>
   );
 };
